b4b1fea52f0398de6176867ed1ff65d40be95b74
IKER MARCELO <IKER MARCELO@LAPTOP-0P8I61QL>
2021-10-11 12:03:02 +0200

    Prueba

--- ProyectoProgIII/src/clss/Prueba.java
+++ ProyectoProgIII/src/clss/Prueba.java
@@ -1,5 +1,7 @@
 package clss;
 
+import java.util.Objects;
+
 public class Prueba {
 	protected int prueba;
 
@@ -15,6 +17,25 @@ public class Prueba {
 	public String toString() {
 		return "Prueba [prueba=" + prueba + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(prueba);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Prueba other = (Prueba) obj;
+		return prueba == other.prueba;
+	}
+	
+	
 	
 	
 
