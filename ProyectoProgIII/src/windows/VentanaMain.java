93baefc467ea448ce0a4db760fcf50600082a45e
imanoldt <imanoldt@localhost>
2021-10-15 12:31:34 +0200

    Escalado del Icono del label menu

--- ProyectoProgIII/src/windows/VentanaMain.java
+++ ProyectoProgIII/src/windows/VentanaMain.java
@@ -1,7 +1,5 @@
 package windows;
 
-
-
 import java.awt.EventQueue;
 import java.awt.*;
 import javax.swing.ImageIcon;
@@ -14,9 +12,9 @@ import javax.swing.JLabel;
 @SuppressWarnings("serial")
 public class VentanaMain extends JFrame {
 
-	private JPanel contentPane, pnlMenu, pnlSuperior,pnlPrincipal;
-	private JLabel lblMenu; 
-
+	private JPanel contentPane, pnlMenu, pnlSuperior, pnlPrincipal;
+	private JLabel lblMenu;
+	private Image imgMenu;
 
 	/**
 	 * Lanza la aplicacion
@@ -37,49 +35,55 @@ public class VentanaMain extends JFrame {
 	/**
 	 * Creacion de la ventana principal
 	 */
-	
+
 	public VentanaMain() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(VentanaMain.class.getResource("/img/IconoAplicacion.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 907, 588); //Medidas de la ventana
+		setBounds(100, 100, 907, 588); // Medidas de la ventana
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		pnlPrincipal = new JPanel();
 		pnlPrincipal.setBackground(Color.LIGHT_GRAY);
 		pnlPrincipal.setBounds(0, 0, 907, 560);
 		contentPane.add(pnlPrincipal);
 		pnlPrincipal.setLayout(null);
-		
+
 		pnlSuperior = new JPanel();
 		pnlSuperior.setBackground(SystemColor.controlHighlight);
 		pnlSuperior.setBounds(0, 0, 907, 50);
 		pnlPrincipal.add(pnlSuperior);
 		SpringLayout sl_pnlSuperior = new SpringLayout();
 		pnlSuperior.setLayout(sl_pnlSuperior);
-		
+
 		lblMenu = new JLabel("");
-		lblMenu.setIcon(new ImageIcon(VentanaMain.class.getResource("/img/icnMenu.png")));
+		sl_pnlSuperior.putConstraint(SpringLayout.SOUTH, lblMenu, 40, SpringLayout.NORTH, pnlSuperior);
+		sl_pnlSuperior.putConstraint(SpringLayout.EAST, lblMenu, 59, SpringLayout.WEST, pnlSuperior);
+		
+//Escalado de la imagen 
+		
+		Image imgMenu = new ImageIcon("src/img/icnMenu.png").getImage();
+		ImageIcon img = new ImageIcon(imgMenu.getScaledInstance(50, 30, Image.SCALE_SMOOTH));
+		lblMenu.setIcon(img);
+
 		sl_pnlSuperior.putConstraint(SpringLayout.NORTH, lblMenu, 10, SpringLayout.NORTH, pnlSuperior);
 		sl_pnlSuperior.putConstraint(SpringLayout.WEST, lblMenu, 10, SpringLayout.WEST, pnlSuperior);
-		sl_pnlSuperior.putConstraint(SpringLayout.SOUTH, lblMenu, 40, SpringLayout.NORTH, pnlSuperior);
-		sl_pnlSuperior.putConstraint(SpringLayout.EAST, lblMenu, 58, SpringLayout.WEST, pnlSuperior);
 		pnlSuperior.add(lblMenu);
-		
+
 		pnlMenu = new JPanel();
 		pnlMenu.setBackground(Color.DARK_GRAY);
 		pnlMenu.setBounds(0, 50, 200, 510);
 		pnlPrincipal.add(pnlMenu);
-		setLocationRelativeTo(null); //Centralizacion de la ventana en la pantalla
-		setTitle("Menu"); //Titulo de la ventana
+		setLocationRelativeTo(null); // Centralizacion de la ventana en la pantalla
+		setTitle("Menu"); // Titulo de la ventana
 		setResizable(false);
 		setVisible(true);
-		
+
 		/**
 		 * EVENTOS
 		 */
-		
+
 	}
 }
