60a1a27ee6be5bbef987ab579b65bf4a693302c2
imanoldt <imanoldt@pc-4-40.customer.ask4.lan>
2021-10-29 13:16:49 +0200

    Ventana Responsive
    Ajustes del JFrame para permitir modificar el tamaño de la ventana sin
    que los paneles se solapen.
    
    Signed-off-by: imanoldt <imanoldt@pc-4-40.customer.ask4.lan>

--- ProyectoProgIII/src/clss/Prueba.java
+++
@@ -1,5 +0,0 @@
-package clss;
-
-public class Prueba {
-
-}
--- ProyectoProgIII/src/windows/VentanaMain.java
+++ ProyectoProgIII/src/windows/VentanaMain.java
@@ -1,39 +1,36 @@
 package windows;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.*;
-import javax.swing.ImageIcon;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.SpringLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+import javax.swing.JMenuBar;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-@SuppressWarnings("serial")
 public class VentanaMain extends JFrame {
 
-	private JPanel contentPane, pnlMenu, pnlSuperior, pnlPrincipal;
-	private JLabel lblMenu;
-	private Image imgMenu, imgCesta;
-	private ImageIcon img, img2;
-	private JPanel pnlSudaderas;
-	private JPanel pnlCamisetas;
-	private JPanel pnlGorras;
-	private JLabel lblCesta;
+	private JPanel contentPane;
+	private JPanel pnlPrincipal;
+	private JPanel pnlNorte;
+	private JPanel pnlIzquierda;
+	private JPanel pnlDerecha;
+	private JLabel lblCamisetas;
+	private JLabel lblPantalones;
+	private JLabel lblZapatillas;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
 
 	/**
-	 * Lanza la aplicacion
+	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					VentanaLogIn login=new VentanaLogIn();
-					login.setVisible(true);
 					VentanaMain frame = new VentanaMain();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -44,110 +41,54 @@ public class VentanaMain extends JFrame {
 	}
 
 	/**
-	 * Creacion de la ventana principal
+	 * Create the frame.
 	 */
-
 	public VentanaMain() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(VentanaMain.class.getResource("/img/IconoAplicacion.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 907, 588); // Medidas de la ventana
+		setBounds(100, 100, 450, 300);
+		setTitle("OutFitShop");
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
 		pnlPrincipal = new JPanel();
-		pnlPrincipal.setBackground(Color.LIGHT_GRAY);
-		pnlPrincipal.setBounds(0, 0, 907, 560);
-		contentPane.add(pnlPrincipal);
-		pnlPrincipal.setLayout(null);
-
-		pnlSuperior = new JPanel();
-		pnlSuperior.setBackground(new Color(183, 29, 57));
-		pnlSuperior.setBounds(0, 0, 907, 50);
-		pnlPrincipal.add(pnlSuperior);
-		SpringLayout sl_pnlSuperior = new SpringLayout();
-		pnlSuperior.setLayout(sl_pnlSuperior);
-
-		lblMenu = new JLabel("");
-
-		// Escalado de la imagen MENU
-
-		imgMenu = new ImageIcon("src/img/icnMenu.png").getImage();
-		img = new ImageIcon(imgMenu.getScaledInstance(50, 30, Image.SCALE_SMOOTH));
-		lblMenu.setIcon(img);
-
-		sl_pnlSuperior.putConstraint(SpringLayout.SOUTH, lblMenu, 40, SpringLayout.NORTH, pnlSuperior);
-		sl_pnlSuperior.putConstraint(SpringLayout.EAST, lblMenu, 59, SpringLayout.WEST, pnlSuperior);
-		sl_pnlSuperior.putConstraint(SpringLayout.NORTH, lblMenu, 10, SpringLayout.NORTH, pnlSuperior);
-		sl_pnlSuperior.putConstraint(SpringLayout.WEST, lblMenu, 10, SpringLayout.WEST, pnlSuperior);
-
-		pnlSuperior.add(lblMenu);
-
-		lblCesta = new JLabel("");
-
-
-		// Escalado de la imagen CESTA
-
-		imgCesta = new ImageIcon("src/img/iconoCesta.png").getImage();
-		img2 = new ImageIcon(imgCesta.getScaledInstance(40, 30, Image.SCALE_SMOOTH));
-		lblCesta.setIcon(img2);
-
-		sl_pnlSuperior.putConstraint(SpringLayout.NORTH, lblCesta, 10, SpringLayout.NORTH, pnlSuperior);
-		sl_pnlSuperior.putConstraint(SpringLayout.WEST, lblCesta, -59, SpringLayout.EAST, pnlSuperior);
-		sl_pnlSuperior.putConstraint(SpringLayout.SOUTH, lblCesta, 0, SpringLayout.SOUTH, lblMenu);
-		sl_pnlSuperior.putConstraint(SpringLayout.EAST, lblCesta, -10, SpringLayout.EAST, pnlSuperior);
-		pnlSuperior.add(lblCesta);
-
-		pnlMenu = new JPanel();
-		pnlMenu.setBackground(Color.DARK_GRAY);
-		pnlMenu.setBounds(0, 50, 200, 510);
-		pnlPrincipal.add(pnlMenu);
-
-		pnlMenu.setLayout(null);
-
-		pnlSudaderas = new JPanel();
-		pnlSudaderas.setBackground(Color.GRAY);
-		pnlSudaderas.setBounds(0, 58, 200, 43);
-		pnlMenu.add(pnlSudaderas);
-
-		pnlCamisetas = new JPanel();
-		pnlCamisetas.setBackground(Color.GRAY);
-		pnlCamisetas.setBounds(0, 113, 200, 43);
-		pnlMenu.add(pnlCamisetas);
-
-		pnlGorras = new JPanel();
-		pnlGorras.setBackground(Color.GRAY);
-		pnlGorras.setBounds(0, 168, 200, 43);
-		pnlMenu.add(pnlGorras);
-		setLocationRelativeTo(null); // Centralizacion de la ventana en la pantalla
-		setTitle("Menu"); // Titulo de la ventana
-		setResizable(false);  //No permite reescalar la ventana
-		setVisible(true);
-
-		/**
-		 * EVENTOS
-		 */
+		contentPane.add(pnlPrincipal, BorderLayout.CENTER);
+		pnlPrincipal.setLayout(new BorderLayout(0, 0));
 		
+		pnlNorte = new JPanel();
+		pnlNorte.setBackground(Color.RED);
+		pnlPrincipal.add(pnlNorte, BorderLayout.NORTH);
+		pnlNorte.setLayout(new GridLayout(0, 4, 0, 0));
 		
+		lblNewLabel_1 = new JLabel("Menu");
+		pnlNorte.add(lblNewLabel_1);
+		
+		pnlIzquierda = new JPanel();
+		pnlIzquierda.setBackground(Color.DARK_GRAY);
+		pnlPrincipal.add(pnlIzquierda, BorderLayout.WEST);
+		pnlIzquierda.setLayout(new GridLayout(4, 0, 0, 0));
+		
+		lblCamisetas = new JLabel("Camisetas");
+		lblCamisetas.setBackground(Color.LIGHT_GRAY);
+		lblCamisetas.setOpaque(true);
 
-		lblMenu.addMouseListener(new MouseAdapter() {//Accion con el click del mouse
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				JOptionPane.showMessageDialog(null, "Informacion", "Informacion", JOptionPane.INFORMATION_MESSAGE);		
-			}
-		});
-		
-		lblCesta.addMouseListener(new MouseAdapter() { //Accion con el click del mouse
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("Prueba");
-			}
-		});
-		
-		/**
-		 * HILOS
-		 */
 
+		pnlIzquierda.add(lblCamisetas);
+		
+		lblPantalones = new JLabel("Pantalones");
+		lblPantalones.setOpaque(true);
+
+		pnlIzquierda.add(lblPantalones);
+		
+		lblNewLabel = new JLabel("Sudaderas");
+		pnlIzquierda.add(lblNewLabel);
+		
+		lblZapatillas = new JLabel("Zapatillas");
+		pnlIzquierda.add(lblZapatillas);
+		
+		pnlDerecha = new JPanel();
+		pnlPrincipal.add(pnlDerecha, BorderLayout.CENTER);
 	}
+
 }
