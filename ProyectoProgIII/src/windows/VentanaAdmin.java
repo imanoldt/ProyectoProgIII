d9fdd59c0f44f2aed36da0b877c5680df6d3ead6
IKER MARCELO <IKER MARCELO@LAPTOP-0P8I61QL>
2021-11-06 13:00:17 +0100

    He empezado la clase admin, voy a corregir las clases

---
+++ ProyectoProgIII/src/windows/VentanaAdmin.java
@@ -0,0 +1,56 @@
+package windows;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class VentanaAdmin extends JFrame{
+	
+	protected JButton anyadirRopa;
+	protected JButton descatalogaRopa;
+	protected JButton guardar;
+	protected JPanel pnlCentral;
+	protected JPanel pnlIzquierda;
+	protected JPanel pnlDerecha;
+	
+	public VentanaAdmin(){
+		Container cp = this.getContentPane();
+		
+		setLocationRelativeTo(null);
+//		setUndecorated(true);
+		setBounds(100, 100, 1039, 549);
+		this.setTitle("Administrador");
+//		this.setResizable(false);
+		this.setVisible(true);
+		this.setSize(600, 500);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);		
+		
+		
+		
+		pnlCentral = new JPanel();
+		pnlCentral.setLayout(new GridLayout(0, 2, 0, 0));
+		cp.add(pnlCentral, BorderLayout.CENTER);
+		
+		pnlDerecha = new JPanel();
+//		pnlDerecha.setBackground(new Color(183, 29, 57));
+		
+		pnlIzquierda = new JPanel();
+		pnlIzquierda.setBackground(new Color(110, 89, 70));
+		
+		pnlCentral.add(pnlIzquierda);
+		pnlCentral.add(pnlDerecha);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new VentanaAdmin();
+	}
+
+}
