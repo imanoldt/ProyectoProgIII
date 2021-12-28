e1fb5a356bc4ce4befec5bbfbf188d8619d2d4e9
imanoldt <imanoldt@localhost>
2021-12-28 19:31:18 +0100

    Pequeña modificacion del panelPrueba

--- ProyectoProgIII/src/paneles/PnlPrueba.java
+++ ProyectoProgIII/src/paneles/PnlPrueba.java
@@ -9,38 +9,48 @@ import clss.Talla;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.Cursor;
 
 public class PnlPrueba extends JPanel {
 	private JLabel lblImagen;
 	private JSpinner spnCantidad;
 	private JButton btnComprar;
 	private JLabel lblNombre;
-	private JComboBox<Talla> comboTalla;
+	private JComboBox<Talla> cbTallas;
+//	private JComboBox<Talla> comboTalla;
 
 	/**
 	 * Create the panel.
 	 */
 	public PnlPrueba() {
-		setLayout(new MigLayout("", "[][][145.00]", "[136.00][][][][]"));
-		
+		setLayout(new MigLayout("", "[][][145.00,grow]", "[136.00][][][][][]"));
+
 		lblImagen = new JLabel("*Imagen*");
+		lblImagen.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblImagen, "cell 1 0 2 1,alignx center,aligny center");
-		
-		lblNombre = new JLabel("______");
-		add(lblNombre, "cell 1 2 2 1,alignx left,aligny center");
-		
+
+		lblNombre = new JLabel("__________________________");
+		add(lblNombre, "cell 1 3 2 1,alignx left,aligny center");
+
 		spnCantidad = new JSpinner();
 		add(spnCantidad, "cell 1 4,growx");
-		
-		btnComprar = new JButton("Comprar");
-		add(btnComprar, "cell 2 4,growx,aligny center");
-		
-		comboTalla = new JComboBox<Talla>();
+
+		cbTallas = new JComboBox<Talla>();
+		add(cbTallas, "cell 2 4,growx");
 
 		for (Talla t : Talla.values()) {
-			comboTalla.addItem(t);
-		add(comboTalla, "cell 3 4,growx,aligny center");
+			cbTallas.addItem(t);
+
 		}
+		
+		btnComprar = new JButton("Comprar");
+		btnComprar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnComprar.setBackground(Color.PINK);
+		add(btnComprar, "cell 1 5 2 1,grow");
 
 	}
 
