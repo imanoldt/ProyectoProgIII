c326c9121ae28bd9dd383510187907df02926a15
IKER MARCELO <IKER MARCELO@LAPTOP-0P8I61QL>
2022-01-05 18:22:59 +0100

    He creado la clase pedido con un contador automático para el numero de pedido

---
+++ ProyectoProgIII/src/clss/Pedido.java
@@ -0,0 +1,39 @@
+package clss;
+
+import java.util.ArrayList;
+
+public class Pedido {
+	public static int contador;
+	private int numero_pedido;
+	private ArrayList<Articulo> articulos;
+	
+	public Pedido(ArrayList<Articulo> articulos) {
+		super();
+		this.articulos = articulos;
+		this.numero_pedido = Pedido.contador;
+		Pedido.contador++;
+	}
+
+	public int getNumero_pedido() {
+		return numero_pedido;
+	}
+
+	public ArrayList<Articulo> getArticulos() {
+		return articulos;
+	}
+
+	public void setArticulos(ArrayList<Articulo> articulos) {
+		this.articulos = articulos;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [numero_pedido=" + numero_pedido + ", articulos=" + articulos + "]";
+	}
+	
+	
+	
+	
+	
+
+}
